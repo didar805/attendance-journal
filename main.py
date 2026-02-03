6cbeea0c4d1344526dd7e535180c218d0bb181da
didar805 <73253055+didar805@users.noreply.github.com>
2026-02-03 12:25:13 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,10 +3,23 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse
 import os
 
-app = FastAPI(title="Attendance Journal API")
+app = FastAPI(title="Журнал посещаемости")
 
+# Подключаем папку frontend
 app.mount("/static", StaticFiles(directory="frontend"), name="static")
 
+# Главная страница
 @app.get("/")
 def home():
     return FileResponse(os.path.join("frontend", "index.html"))
+
+# Список студентов
+students = [
+    {"id": 1, "name": "Баллыева Б.", "role": "студент"},
+    {"id": 2, "name": "Бердиев Р.", "role": "студент"},
+    {"id": 3, "name": "Назаров Дидар", "role": "администратор"},
+]
+
+@app.get("/students")
+def get_students():
+    return students

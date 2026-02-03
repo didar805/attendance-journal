79ebb7847c1ed91079f18711d8d10c7d1004ec66
didar805 <73253055+didar805@users.noreply.github.com>
2026-02-03 14:28:53 +0300

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -7,13 +7,22 @@ async function loadStudents() {
 
     students.forEach(s => {
         const div = document.createElement("div");
+
         div.innerHTML =
-    s.name + " (" + s.role + ") " +
-    <button onclick="deleteStudent(${s.id})">Удалить</button>;
+            s.name + " (" + s.role + ") " +
+            <button onclick="deleteStudent(${s.id})">Удалить</button>;
+
+        container.appendChild(div);
+    });
+}
+
 async function addStudent() {
     const name = document.getElementById("newStudent").value.trim();
 
-    if (name === "") return;
+    if (name === "") {
+        alert("Введите имя студента");
+        return;
+    }
 
     await fetch("/students/add?name=" + name, { method: "POST" });
 
@@ -21,8 +30,9 @@ async function addStudent() {
     loadStudents();
 }
 
-window.onload = loadStudents;
 async function deleteStudent(id) {
     await fetch("/students/" + id, { method: "DELETE" });
     loadStudents();
 }
+
+window.onload = loadStudents;

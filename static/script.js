0e0034a5d90d76043fb949ebd0bd0340e8ac6711
didar805 <73253055+didar805@users.noreply.github.com>
2026-02-03 18:10:52 +0300

    Add files via upload

---
+++ static/script.js
@@ -0,0 +1,59 @@
+async function loadStudents() {
+    const response = await fetch("/students");
+    const students = await response.json();
+
+    const container = document.getElementById("students");
+    container.innerHTML = "";
+
+    students.forEach(s => {
+        const div = document.createElement("div");
+        div.className = "student-row";
+
+        const text = document.createElement("span");
+        text.innerText = s.name + " (" + s.role + ")";
+
+        const btn = document.createElement("button");
+        btn.className = "icon-btn";
+
+        const img = document.createElement("img");
+        img.src = "/static/delete.svg";
+        img.width = 18;
+
+        btn.appendChild(img);
+        btn.onclick = () => deleteStudent(s.id);
+
+        div.appendChild(text);
+        div.appendChild(btn);
+
+        container.appendChild(div);
+    });
+}
+
+async function addStudent() {
+    const nameInput = document.getElementById("newStudent");
+    const name = nameInput.value.trim();
+
+    if (!name) {
+        alert("Введите имя");
+        return;
+    }
+
+    await fetch("/students/add?name=" + encodeURIComponent(name), {
+        method: "POST"
+    });
+
+    nameInput.value = "";
+    loadStudents();
+}
+
+async function deleteStudent(id) {
+    if (!confirm("Удалить студента?")) return;
+
+    await fetch("/students/" + id, {
+        method: "DELETE"
+    });
+
+    loadStudents();
+}
+
+window.onload = loadStudents;

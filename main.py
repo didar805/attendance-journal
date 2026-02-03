4baf65e75ef301acd79ca56221d971c1e18b3ec3
didar805 <73253055+didar805@users.noreply.github.com>
2026-02-03 12:43:14 +0300

    Update main.py

--- main.py
+++ main.py
@@ -23,3 +23,24 @@ students = [
 @app.get("/students")
 def get_students():
     return students
+students = [
+    {"id": 1, "name": "Баллыева Б.", "role": "студент"},
+    {"id": 2, "name": "Бердиев Р.", "role": "студент"},
+    {"id": 3, "name": "Назаров Дидар", "role": "администратор"},
+]
+
+@app.get("/students")
+def get_students():
+    return students
+
+@app.post("/students/add")
+def add_student(name: str):
+    new_id = len(students) + 1
+    students.append({"id": new_id, "name": name, "role": "студент"})
+    return {"status": "ok"}
+
+@app.delete("/students/{student_id}")
+def delete_student(student_id: int):
+    global students
+    students = [s for s in students if s["id"] != student_id]
+    return {"status": "deleted"}

5dc111ee79ec9c75cb5f6e3cfffaab97f1f8fd36
didar805 <73253055+didar805@users.noreply.github.com>
2026-02-03 12:30:33 +0300

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -1,1 +1,15 @@
-// JS placeholder
+async function loadStudents() {
+    const response = await fetch("/students");
+    const students = await response.json();
+
+    const container = document.getElementById("students");
+    container.innerHTML = "";
+
+    students.forEach(s => {
+        const div = document.createElement("div");
+        div.innerText = s.name + " (" + s.role + ")";
+        container.appendChild(div);
+    });
+}
+
+window.onload = loadStudents;

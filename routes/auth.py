630848891eac66844601e4e4f1dd6859ae3e4ade
didar805 <73253055+didar805@users.noreply.github.com>
2026-02-07 20:41:10 +0300

    Update auth.py

--- routes/auth.py
+++ routes/auth.py
@@ -1,7 +1,24 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Form
 
 router = APIRouter()
 
+# wagtlaýyn ulanyjy bazasy
+users = {
+    "didarnazarov838@gmail.com": {
+        "password": "2oh",
+        "roles": ["admin", "student"]
+    }
+}
+
 @router.post("/login")
-def login():
-    return {"status": "ok"}
+def login(username: str = Form(...), password: str = Form(...)):
+    user = users.get(username)
+
+    if not user or user["password"] != password:
+        return {"status": "error", "message": "Login yalnys"}
+
+    return {
+        "status": "ok",
+        "email": username,
+        "roles": user["roles"]
+    }

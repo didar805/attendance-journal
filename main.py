1206caa54db23e14c9b5b0c4503a31acbfee7e8e
didar805 <73253055+didar805@users.noreply.github.com>
2026-02-07 13:48:58 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from datetime import date
 import os
 
 app = FastAPI()
-from attendance_journal.auth import router as auth_router
+from auth import router as auth_router
 app.include_router(auth_router)
 app.mount("/static", StaticFiles(directory="static"), name="static")
 

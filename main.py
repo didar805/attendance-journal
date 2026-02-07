0be0f27ebc82663b01f1450b0d0b10787011713e
didar805 <73253055+didar805@users.noreply.github.com>
2026-02-07 23:33:33 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,11 @@ from fastapi.responses import FileResponse
 from routes.auth import router as auth_router
 
 app = FastAPI()
-
+students = [
+    {"name": "Ali", "group": "A1"},
+    {"name": "Veli", "group": "A1"},
+    {"name": "Aysha", "group": "B2"}
+]
 app.include_router(auth_router)
 app.mount("/static", StaticFiles(directory="static"), name="static")
 

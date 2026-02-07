cc848729585681db99a6f906691f299de9c39d4b
didar805 <73253055+didar805@users.noreply.github.com>
2026-02-07 14:18:27 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from fastapi.responses import FileResponse
 from datetime import date
 import os
 
-from auth import router as auth_router
+from backend.auth import router as auth_router
 app = FastAPI()
 app.include_router(auth_router)
 app.mount("/static", StaticFiles(directory="static"), name="static")
